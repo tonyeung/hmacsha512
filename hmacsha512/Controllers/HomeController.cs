3a88413c7203d08330252e6cb3d3acd0eee96c8a
ton.yeung <tyeung@auto-master.com>
2013-06-28 11:00:10 -0500

    init commit

---
+++ hmacsha512/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hmacsha512.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult hmac(string sig, string token)
+        {
+            var signature = string.Empty;
+            using (var hmac = new HMACSHA512(Encoding.UTF8.GetBytes(token)))
+            {
+                var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(sig));
+                signature = Convert.ToBase64String(hash);
+            }
+
+            return Json(signature);
+        }
+    }
+}
---
+++ hmacsha512/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hmacsha512.MvcApplication" Language="C#" %>
